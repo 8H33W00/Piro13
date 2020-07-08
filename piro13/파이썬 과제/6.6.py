333c6e84e8f6a8f3378a1b62632b1854237b3aab
8H33W00 <tigerson95@gmail.com>
2020-07-08 20:24:16 +0900

    파이썬 코딩도장 퀴즈

---
+++ piro13/파이썬 과제/6.6.py
@@ -0,0 +1,26 @@
+#a, b, c = map(int, input().split())
+#print(a + b + c)
+
+#a = 50
+#b = 100
+#c = None
+
+#print(a)
+#print(b)
+#print(c)
+
+#a , b, c, d = map(int, input().split())
+#print(int((a + b + c + d) / 4))
+
+#print(16, 9, sep=':')
+
+#ear, month, day, hour, minute, second = input().split()
+
+#print(year, month,day, sep='-', end='T')
+#print(hour, minute, second, sep=':')
+
+#a, b, c, d = map(int, input().split())
+#print(a >= 90 and b > 80 and c > 85 and d >= 80)
+
+a = int(input())
+print(tuple(range(-10, 10, a)))
